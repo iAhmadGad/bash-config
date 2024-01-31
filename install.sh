1ad72bbd098c77e00685cc556f4068beef9454d5
iAhmadGad <ahmadasaadgad@gmail.com>
2024-01-31 21:59:29 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 echo -e "\033[01;36mInstalling Bash configurations..\033[0m"
-printf "\n# Source Bash configurations\nsource ~/.bash-config/custom.sh\nsource ~/.bash-config/aliases.sh\n" >> ~/.bashrc
+printf "\n# Load Bash configurations\nsource ~/.bash-config/main.sh\n" >> ~/.bashrc
 echo -e "\033[01;32mInstalled successfully!\033[0m"
 echo -e "\033[01;97mStart a new session or execute source ~/.bashrc\033[0m"
