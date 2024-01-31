5dee0c9afaf1637191456fa881498927beb9b672
iAhmadGad <ahmadasaadgad@gmail.com>
2024-01-31 21:38:08 +0300

    Add local files

---
+++ aliases.sh
@@ -0,0 +1,4 @@
+alias pkgupdate="sudo apt update"
+alias pkgupgrade="sudo apt upgrade"
+alias pkgin="sudo apt install"
+alias clean="sudo apt clean && sudo apt autoclean && sudo apt autoremove"
---
+++ install.sh
@@ -0,0 +1,1 @@
+printf "\n# Source Bash configurations\nsource ~/.bash-config/custom.sh\nsource ~/.bash-config/aliases.sh\n" >> ~/.bashrc
