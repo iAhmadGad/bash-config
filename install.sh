7a16738fb9e38075e4482fc0f09db17b4fb0b3d2
iAhmadGad <ahmadasaadgad@gmail.com>
2024-02-01 17:48:23 +0000

    Edit install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
-echo -e "\033[01;36mInstalling Bash configurations..\033[0m"
-printf "\n# Load Bash configurations\nsource ~/.bash-config/main.sh\n" >> ~/.bashrc
-echo -e "\033[01;32mInstalled successfully!\033[0m"
-echo -e "\033[01;97mStart a new session or execute source ~/.bashrc\033[0m"
+install_bash_config="source ~/.bash-config/main.sh"
+if grep -q "$install_bash_config" ~/.bashrc
+then
+    echo -e "\033[01;32mBash Configuration already installed!\033[0m"
+else
+    echo -e "\033[01;36mInstalling Bash configurations..\033[0m"
+    printf "\n# Load Bash Configurations\n$install_bash_config\n" >> ~/.bashrc
+    echo -e "\033[01;32mInstalled successfully!\033[0m"
+    echo -e "\033[01;97mStart a new session or execute \"source ~/.bashrc\"\033[0m"
+fi
